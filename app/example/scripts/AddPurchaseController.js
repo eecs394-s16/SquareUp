3ae2f9057ad4c7ce37405cfbf9082b89b8a64898
jameswhang <sungyoonwhang2017@u.northwestern.edu>
2016-04-06 00:14:15 -0500

    Added a way to add people's name in the purchase view

--- app/example/scripts/AddPurchaseController.js
+++ app/example/scripts/AddPurchaseController.js
@@ -1,14 +1,21 @@
 angular
     .module('example')
-    .controller('AddPurchaseController', function($scope, supersonic) {
+    .controller('AddPurchaseController', ['$scope', 'supersonic', function($scope, supersonic) {
         $scope.navbarTitle = "Add a Purchase";
 
-        $scope.addPersonTextBox = function() {
-            console.log("testing add person box");
-            $('.list-people').append($compile('<label class="item item-input"><span class="input-label">Price</span><input type="text" placeholder="How much was it?"></label>')(scope));
-            scope.$apply();
+        $scope.squareUp = function() {
+            var options = {
+              message: "A longer message with \n\n\n\nmultiple lines.",
+              buttonLabel: "Close"
+            };
+
+            supersonic.ui.dialog.alert("Custom title!", options).then(function() {
+              supersonic.logger.log("Alert closed.");
+            });
         }
 
-
-
-    });
+        $scope.addPerson = function() {
+            var myEl = angular.element( document.querySelector( '.list-people' ) );
+            myEl.append('<label class="item item-input"><span class="input-label">Share With:</span><input type="text" placeholder="Who are you sharing it with?"></label>');
+        }
+    }]);
