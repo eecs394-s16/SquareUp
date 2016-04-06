6b5d1084b1ab6e55e8a8f1bba5e761dad942accf
jameswhang <sungyoonwhang2017@u.northwestern.edu>
2016-04-06 01:46:08 -0500

    Added Firebase backend

--- app/example/scripts/AddPurchaseController.js
+++ app/example/scripts/AddPurchaseController.js
@@ -1,15 +1,38 @@
 angular
     .module('example')
+/*
+    .factory('Purchases', ['$firebaseArray', function($firebaseArray) {
+        return $firebaseArray(new Firebase('https://squareup-split.firebaseio.com/purchases'));
+    }])*/
+
     .controller('AddPurchaseController', ['$scope', 'supersonic', function($scope, supersonic) {
         $scope.navbarTitle = "Add a Purchase";
-
         $scope.squareUp = function() {
+            supersonic.logger.log("clicked squareUp.");
+
+            var PurchasesRef = new Firebase('https://squareup-split.firebaseio.com/purchases');
+            var purchase = {
+                ownerID: $scope.data.ownerID,
+                itemName: $scope.data.itemName,
+                price: $scope.data.price,
+            };
+            supersonic.logger.log("about to push.");
+            supersonic.logger.log(purchase);
+
+
+            var newPurchaseRef = PurchasesRef.push().set(purchase);
+            supersonic.logger.log(newPurchaseRef);
+            supersonic.logger.log("done pushing.");
+
+
+
+
             var options = {
-              message: "A longer message with \n\n\n\nmultiple lines.",
-              buttonLabel: "Close"
+              message: "Your friends received your bill!",
+              buttonLabel: "Ok"
             };
 
-            supersonic.ui.dialog.alert("Custom title!", options).then(function() {
+            supersonic.ui.dialog.alert("Squared it up!", options).then(function() {
               supersonic.logger.log("Alert closed.");
             });
         }
