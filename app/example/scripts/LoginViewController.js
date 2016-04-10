f83e35441c90f398ad00c2dc2920635e3eebf043
jameswhang <sungyoonwhang2017@u.northwestern.edu>
2016-04-10 04:18:35 -0500

    Added Facebook login

--- app/example/scripts/LoginViewController.js
+++ app/example/scripts/LoginViewController.js
@@ -1,14 +1,69 @@
 angular
     .module('example')
-    .factory('Auth', ['$firebaseAuth', function ($firebaseAuth) {
-        return new Firebase('https://squareup-split.firebaseio.com/users');
-    }])
-    .controller('LoginViewController', ['$scope', 'supersonic', 'Auth', function($scope, supersonic, Auth) {
+    .controller('LoginViewController', ['$scope', 'supersonic', '$firebaseAuth', function($scope, supersonic, $firebaseAuth) {
         $scope.moveOn = function() {
             var animation = supersonic.ui.animate("flipHorizontalFromRight");
             supersonic.ui.initialView.dismiss(animation);
         }
+
+        $scope.Auth = function() {
+            var ref = new Firebase('https://squareup-split.firebaseio.com/users');
+            return $firebaseAuth(ref);
+        }
+        $scope.loginFB = function() {
+            var ref = new Firebase('https://squareup-split.firebaseio.com');
+            var userRef = new Firebase('https://squareup-split.firebaseio.com/users');
+            ref.authWithOAuthPopup("facebook", function(err, authData) {
+                if (err) {
+                    supersonic.logger.log("login failed! with err" + err);
+                } else {
+                    supersonic.logger.log("Authenticated successfully !!");
+                    $scope.userData = authData;
+                    userRef.push(authData);
+                    $scope.moveOn();
+                }
+            });
+        }
         $scope.login = function() {
-            $scope.moveOn();
+            var Auth = new Firebase('https://squareup-split.firebaseio.com/users');
+            Auth.authWithPassword({
+                username: $scope.data.username,
+                password: $scope.data.password
+            }).then(function(authData) {
+                $scope.moveOn();
+            }).catch(function(error) {
+                var options = {
+                    message: err,
+                    buttonLabel: "Ok"
+                };
+                supersonic.ui.dialog.alert("Failed to Login", options).then(function() {
+                    supersonic.logger.log("Alert closed.");
+                });
+            });
+        }
+        $scope.signup = function() {
+            supersonic.logger.log("clicked signed up");
+            var Auth = $scope.Auth();
+            Auth.createUser({
+                email: $scope.data.email,
+                password: $scope.data.password,
+            }).then(function(userData) {
+                supersonic.logger.log("made user");
+                $scope.usersRef.child(userData.uid).set({
+                    provider: 'password',
+                    username: $scope.data.username,
+                });
+                $scope.login();
+            }).catch(function(err) {
+                supersonic.logger.log("error");
+                var options = {
+                    message: err,
+                    buttonLabel: "Ok"
+                };
+                supersonic.ui.dialog.alert("Failed to Sign Up", options).then(function() {
+                    supersonic.logger.log("Alert closed.");
+                });
+            });
+            supersonic.logger.log("done");
         }
     }]);
