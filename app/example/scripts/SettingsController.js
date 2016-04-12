e98ef7f057e30b6c9cc04bc611fead3657a01b88
cobrien7 <connoro7168@gmail.com>
2016-04-12 16:47:41 -0500

    summary page

--- app/example/scripts/SettingsController.js
+++ app/example/scripts/SettingsController.js
@@ -1,5 +1,51 @@
 angular
-  .module('example')
-  .controller('SettingsController', function($scope, supersonic) {
-    $scope.navbarTitle = "Settings";
-  });
+    .module('example')
+    .controller('SettingsController', ['$scope', 'supersonic', function($scope, supersonic) {
+	//$scope.navbarTitle = "Settings";
+	$scope.doRefresh = function(){
+	    supersonic.logger.log("Summary refresh called.");
+	    $scope.balances = {};
+	    $scope.positives = {};
+	    var changeBalance = function(splitter,purchase,sign){
+		var change = sign * Number(purchase["price"]) / Number(purchase["numPpl"]);
+		if (splitter in $scope.balances){
+		    $scope.balances[splitter] += change;
+		}
+		else {
+		    $scope.balances[splitter] = change;
+		}
+	    };
+	    var addCredits = function(snapshot, prevKey){
+		supersonic.logger.log("addCredits called.");
+		var purchase = snapshot.val();
+		for (var splitter in purchase["splitWith"]){
+		    changeBalance(splitter,purchase,1);
+		    supersonic.logger.log($scope.balances[splitter]);
+		    supersonic.logger.log(splitter);
+		}
+	    };
+	    var addDebts = function(purchID){
+		supersonic.logger.log("addDebts called.");
+		supersonic.logger.log(purchID.key());
+		var PurchaseRef = new Firebase('https://boiling-torch-3709.firebaseio.com/purchases/'.concat(purchID.key()));
+
+		PurchaseRef.on("value",function(snapshot){
+		    supersonic.logger.log("This happens at least once");
+		    var purchase = snapshot.val();
+		    changeBalance(purchase["buyer"],purchase,-1);
+		    supersonic.logger.log($scope.balances[purchase["buyer"]]);
+		    supersonic.logger.log(purchase["buyer"]);
+
+		});
+
+	    };
+	    //TODO: eliminate hardcoding chris3 here
+	    var creditRef = new Firebase('https://boiling-torch-3709.firebaseio.com/purchases');
+	    creditRef.orderByChild("buyer").equalTo("chris3").on("child_added",addCredits);
+
+	    var debtRef = new Firebase('https://boiling-torch-3709.firebaseio.com/profiles/chris3/partyToPurchase');
+	    debtRef.on("value",function(purchases){ purchases.forEach(addDebts); } );
+	    $scope.$apply();
+	};
+	$scope.doRefresh();
+    }]);
